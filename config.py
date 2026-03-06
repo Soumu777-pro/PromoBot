10630d42d9a55479bc5576d819312e48e7bb2bf3
Soumu777-pro <soumyadeepdeep42@gmail.com>
2026-03-06 12:14:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,70 @@
+==============================
+
+Telegram API (from my.telegram.org)
+
+==============================
+
+API_ID = "API_ID"
+API_HASH = "YOUR_API_HASH_HERE"
+
+==============================
+
+Bot Tokens
+
+==============================
+
+STARTUP_BOT_TOKEN = "STARTUP_BOT_TOKEN_HERE"
+APPROVAL_BOT_TOKEN = "APPROVAL_BOT_TOKEN_HERE"
+CONTROL_BOT_TOKEN = "CONTROL_BOT_TOKEN_HERE"
+
+==============================
+
+Owner & Admin
+
+==============================
+
+OWNER_ID = 8229785450
+
+Sudo users who can use admin commands
+
+SUDO_USERS = [
+8229785450
+]
+
+==============================
+
+Sessions
+
+==============================
+
+SESSION_FOLDER = "sessions"
+
+==============================
+
+Verification system
+
+==============================
+
+VERIFY_CODES = {}
+
+login expiry (3 days)
+
+LOGIN_EXPIRY_SECONDS = 259200
+
+==============================
+
+Auto Promo Default
+
+==============================
+
+DEFAULT_PROMO_TIME = 600   # 10 minutes
+
+==============================
+
+Bots usernames (optional)
+
+==============================
+
+STARTUP_BOT_USERNAME = "@YourStartupBot"
+APPROVAL_BOT_USERNAME = "@YourApprovalBot"
+CONTROL_BOT_USERNAME = "@YourControlBot"
