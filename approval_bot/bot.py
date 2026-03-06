1782e8279761dc04c0b3498099efb54054d52fa6
Soumu777-pro <soumyadeepdeep42@gmail.com>
2026-03-06 13:20:11 +0530

    Update bot.py

--- approval_bot/bot.py
+++ approval_bot/bot.py
@@ -1,13 +1,14 @@
+# approval_bot/bot.py
 from pyrogram import Client
 import config
 
 app = Client(
-"approval-bot",
-api_id=config.API_ID,
-api_hash=config.API_HASH,
-bot_token=config.APPROVAL_BOT_TOKEN
+    "approval-bot",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.APPROVAL_BOT_TOKEN
 )
 
-print("Approval Bot Running...")
+print("✅ Approval Bot Running...")
 
 app.run()
